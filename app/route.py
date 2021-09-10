3521ec55149aeccfa31259c072550c844dc6d580
Ligang Zhou (c) <zligang@vmware.com>
2021-09-10 23:16:20 +0800

    add /mloot/random3

--- app/route.py
+++ app/route.py
@@ -1,8 +1,9 @@
+import random
+
 from flask import Blueprint, jsonify
 from exception import APIParamError
 from model import MLoot
 from utils import get_image
-
 
 router_bp = Blueprint('router', __name__)
 
@@ -12,7 +13,7 @@ def nft(_token_id):
     if _token_id <= 0 or _token_id > 10000:
         raise APIParamError()
 
-    mloot = MLoot.query.filter(MLoot.index==_token_id).scalar()
+    mloot = MLoot.query.filter(MLoot.index == _token_id).scalar()
     mloot = {
         'name': mloot.name,
         'description': mloot.description,
@@ -20,3 +21,16 @@ def nft(_token_id):
         'attributes': mloot.attributes,
     }
     return jsonify(mloot)
+
+
+@router_bp.route('/mloot/random3')
+def random3():
+    idx = random.randint(1, 10000 - 2)
+    models = MLoot.query.filter(MLoot.index.in_(tuple([idx, idx+1, idx+2]))).all()
+    mloots = []
+    for model in models:
+        mloots.append({
+            'id': model.index,
+            'attributes': model.word_list
+        })
+    return jsonify(mloots)
